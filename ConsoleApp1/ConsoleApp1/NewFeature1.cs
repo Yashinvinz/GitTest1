b47396466ccb19885822a07033cb9fcdbed696e8
Yashinvinz <yashwanthjv1993@gmail.com>
2019-02-08 21:43:48 +0530

    Update NewFeature1.cs
    change int to var

--- ConsoleApp1/ConsoleApp1/NewFeature1.cs
+++ ConsoleApp1/ConsoleApp1/NewFeature1.cs
@@ -10,9 +10,9 @@ namespace ConsoleApp1
     {
         public int Add()
         {
-            int x = 2;
-            int y = 4;
-            int z = x + y;
+            var x = 2;
+            var y = 4;
+            var z = x + y;
             return z;
         }
     }

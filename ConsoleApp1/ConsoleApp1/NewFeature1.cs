699604107ebfd4ea62a53499d122364b5366e38a
yashinvinz <yashwanthjv1993@gmail.com>
2019-02-08 21:42:21 +0530

    Added add code

--- ConsoleApp1/ConsoleApp1/NewFeature1.cs
+++ ConsoleApp1/ConsoleApp1/NewFeature1.cs
@@ -8,5 +8,12 @@ namespace ConsoleApp1
 {
     class NewFeature1
     {
+        public int Add()
+        {
+            int x = 2;
+            int y = 4;
+            int z = x + y;
+            return z;
+        }
     }
 }

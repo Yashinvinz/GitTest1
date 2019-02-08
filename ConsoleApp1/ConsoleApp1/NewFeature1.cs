c62f761069bd6e936b31de50ac0d3dac7be291f6
Yashinvinz <yashwanthjv1993@gmail.com>
2019-02-08 21:48:17 +0530

    Update NewFeature1.cs

--- ConsoleApp1/ConsoleApp1/NewFeature1.cs
+++ ConsoleApp1/ConsoleApp1/NewFeature1.cs
@@ -8,12 +8,6 @@ namespace ConsoleApp1
 {
     class NewFeature1
     {
-        public int Add()
-        {
-            var x = 2;
-            var y = 4;
-            var z = x + y;
-            return z;
-        }
+    
     }
 }
